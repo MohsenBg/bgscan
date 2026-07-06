8f074f1f1e20f92919bf747bb927641083d1cfae
MohsenBg <moh.1380.1393@gmail.com>
2026-07-06 23:56:50 +0330

    fix: change port number conversion to use ParseUint for better error handling

--- internal/ui/components/inspector/dns/model.go
+++ internal/ui/components/inspector/dns/model.go
@@ -204,7 +204,7 @@ func New(l *layout.Layout, name string) *Model {
 		"Enter port number",
 		int(r.Port),
 		func(v string) error {
-			n, err := strconv.Atoi(v)
+			n, err := strconv.ParseUint(v, 10, 16)
 			if err != nil {
 				return err
 			}
